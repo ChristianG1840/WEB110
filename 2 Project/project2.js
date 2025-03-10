088ce931e75bf97b4ca8eb3199b4533354c2dfc7
ChristianG1840 <christiang1840@gmail.com>
2025-03-10 00:00:16 -0500

    project2.js

--- 2 Project/project2.js
+++ 2 Project/project2.js
@@ -1,20 +1,20 @@
 // Christian Gutierrez
 // 3/8/25
 
-let myName = "Your Full Name";
 
-document.getElementById("p1").textContent = myName;
+let myName = "Christian Gutierrez";
+document.getElementById("p1").textContent = myName; 
 
-let n1 = 10;
-let n2 = 5;
+let n1 = 5;
+let n2 = 10;
 
-document.getElementById("p2").textContent = n1 + n2;
+document.getElementById("p2").textContent = n1 + n2; 
 
-document.getElementById("p3").textContent = n1 * n2;
+document.getElementById("p3").textContent = n1 * n2; 
 
-document.getElementById("p4").textContent = myName + n1;
+document.getElementById("p4").textContent = myName + n1; 
 
-document.getElementById("p5").textContent = myName * n1;
+document.getElementById("p5").textContent = myName * n1; 
 
-let myAge = 25; 
-document.getElementById("p6").textContent = myAge > (n1 * n2);
+let myAge = 21; 
+document.getElementById("p6").textContent = myAge > (n1 * n2); 
