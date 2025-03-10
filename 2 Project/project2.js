33ce949958504322fbbc57996c38c462866c7240
ChristianG1840 <christiang1840@gmail.com>
2025-03-09 23:51:08 -0500

    Create project2.js

---
+++ 2 Project/project2.js
@@ -0,0 +1,20 @@
+// Christian Gutierrez
+// 3/8/25
+
+let myName = "Your Full Name";
+
+document.getElementById("p1").textContent = myName;
+
+let n1 = 10;
+let n2 = 5;
+
+document.getElementById("p2").textContent = n1 + n2;
+
+document.getElementById("p3").textContent = n1 * n2;
+
+document.getElementById("p4").textContent = myName + n1;
+
+document.getElementById("p5").textContent = myName * n1;
+
+let myAge = 25; 
+document.getElementById("p6").textContent = myAge > (n1 * n2);
